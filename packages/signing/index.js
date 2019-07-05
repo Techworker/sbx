68b63458fe8fbcf4e70535d99df9c128815b18ec
techworker <benjaminansbach@gmail.com>
2019-07-06 01:47:50 +0200

    added easy access function to make creation of data ops simpler, will add others

--- packages/signing/index.js
+++ packages/signing/index.js
@@ -1,3 +1,5 @@
+const Operations = require('./src/Operations');
+
 module.exports = {
   Signer: require('./src/Signer'),
   RawOperations: require('./src/RawOperations'),
@@ -5,5 +7,23 @@ module.exports = {
   Coding: {
     PublicKeyWithLength: require('./src/Coding/PublicKeyWithLength')
   },
-  Operations: require('./src/Operations')
+  Operations,
+  /**
+   *
+   * @param signer
+   * @param sender
+   * @param target
+   * @return {Data}
+   */
+  createDataOperation(signer, sender = null, target = null) {
+    if (sender === null) {
+      sender = signer;
+    }
+
+    if (target === null) {
+      target = sender;
+    }
+
+    return new Operations.Data.Operation(signer, sender, target);
+  }
 };
